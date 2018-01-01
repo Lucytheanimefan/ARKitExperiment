4ad986e14a948d209980fc96b408702e53c344c3
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-01-01 17:50:47 -0500

    Make a box

---
+++ ARKitExperiment/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  ARKitExperiment
+//
+//  Created by Lucy Zhang on 1/1/18.
+//  Copyright © 2018 Lucy Zhang. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addBox()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+    
+    func addBox(){
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3Make(0, 0, -0.2)
+        
+        sceneView.scene.rootNode.addChildNode(boxNode)
+    }
+
+
+}
+
