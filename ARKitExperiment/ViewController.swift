c24b893c20831c07b8a2b1a4d41e74df357cf333
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-01-01 21:30:05 -0500

    Add boxes to the world

--- ARKitExperiment/ViewController.swift
+++ ARKitExperiment/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         addBox()
+        addTapGestureToSceneView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,16 +35,42 @@ class ViewController: UIViewController {
         sceneView.session.pause()
     }
     
-    func addBox(){
+    func addBox(x: Float = 0, y: Float = 0, z: Float = -0.2){
         let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         
         let boxNode = SCNNode()
         boxNode.geometry = box
-        boxNode.position = SCNVector3Make(0, 0, -0.2)
+        boxNode.position = SCNVector3Make(x, y, z)
         
         sceneView.scene.rootNode.addChildNode(boxNode)
     }
 
+    func addTapGestureToSceneView(){
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTap(withGestureRecognizer:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func didTap(withGestureRecognizer recognizer: UIGestureRecognizer) {
+        let tapLocation = recognizer.location(in: sceneView)
+        let hitTestResults = sceneView.hitTest(tapLocation)
+        guard let node = hitTestResults.first?.node else {
+            let hitTestResultsWithFeaturePoints = sceneView.hitTest(tapLocation, types: .featurePoint)
+            
+            if let hitTestResultWithFeaturePoints = hitTestResultsWithFeaturePoints.first {
+                let translation = hitTestResultWithFeaturePoints.worldTransform.translation
+                addBox(x: translation.x, y: translation.y, z: translation.z)
+            }
+            return
+            
+        }
+        node.removeFromParentNode()
+    }
 
 }
 
+extension float4x4 {
+    var translation: float3 {
+        let translation = self.columns.3
+        return float3(translation.x, translation.y, translation.z)
+    }
+}
